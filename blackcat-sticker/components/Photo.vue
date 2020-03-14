348bb3be3f62cc8e53a78012b7c3e6caa529d543
Aleksandra Foksman <aleksandrafoksman@aleksandras-mbp.home>
2020-03-13 21:31:11 -0700

    work on drag

--- blackcat-sticker/components/Photo.vue
+++ blackcat-sticker/components/Photo.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="photo">
     <img :src="getSrc" class="img"/>
-    <div class="canvas">{{ getLabel }}</div>
+    <div class="canvas" @dragover="allowDrop" @drop="drop" id="labelbox">
+      <p id="labeltarget"
+       :style="{position: 'absolute', left: getX, top: getY}"
+       @mousedown="down"
+       draggable="true">
+        {{ getLabel }}
+        </p>
+    </div>
   </div>
 </template>
 
@@ -13,8 +20,50 @@ export default {
         },
         getLabel(){
           return this.$store.state.label.labelText
+        },
+        getX(){
+          return this.$store.state.label.x
+        },
+        getY(){
+          return this.$store.state.label.y
         }
     },
+    methods:{
+      dragStart(e){
+        e.dataTransfer.setData('Label', e.target.id)
+      },
+      allowDrop(e){
+        e.preventDefault()
+      },
+      moveAt(e, pageX, pageY){
+        let labelElement = document.getElementById('labelbox').getBoundingClientRect()
+        let shiftX = e.clientX - labelElement.left;
+        let shiftY = e.clientY - labelElement.top;
+        const coords = {
+          x: shiftX + 'px',
+          y: shiftY + 'px'
+        }
+        this.$store.commit('label/sendXYLabel', coords)
+      },
+      move(e){
+         this.moveAt(e.pageX, e.pageY)  
+      },
+      down(e){
+        let labelElement = document.getElementById('labelbox').getBoundingClientRect()
+        let shiftX = e.clientX - labelElement.left;
+        let shiftY = e.clientY - labelElement.top;
+        console.log('clientX ', e.clientX, '; clientY ', e.clientY)
+        console.log('shiftX ', shiftX ,'; shiftY ', shiftY)
+        console.log('pageX ', e.pageX, '; pageY ', e.pageY)
+        const coords = {
+          x: shiftX + 'px',
+          y: shiftY  + 'px'
+        }
+        this.$store.commit('label/sendXYLabel', coords)
+        document.addEventListener('mousemove', this.move);
+
+      }
+    }
 }
 </script>
 
