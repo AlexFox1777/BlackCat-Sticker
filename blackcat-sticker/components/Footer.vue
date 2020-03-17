e77ff4df4497ae6bab48d4224925bbd35f45a0e2
Aleksandra Foksman <aleksandrafoksman@aleksandras-mbp.home>
2020-03-16 20:52:53 -0700

    logo mini animation

---
+++ blackcat-sticker/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="footer">
+      © Developed & Designed by Aleksandra Foksman
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.footer{
+    margin-top: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #181743;
+    color: white;
+    height: 100px;
+    min-width: 200px;
+    width: 100%;
+}
+</style>
